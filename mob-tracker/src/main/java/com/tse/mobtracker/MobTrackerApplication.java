533f90ac4e3cd19a35bd0aee8177be8995eac075
VThomas-git <thomas.vang92@gmail.com>
2021-02-16 16:10:18 +0100

    Init project back

---
+++ mob-tracker/src/main/java/com/tse/mobtracker/MobTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tse.mobtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobTrackerApplication.class, args);
+	}
+
+}
---
+++ mob-tracker/src/test/java/com/tse/mobtracker/MobTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tse.mobtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
