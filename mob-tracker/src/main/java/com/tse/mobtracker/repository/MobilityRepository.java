61b036d36acf1f363f19df00074ba813d6671d5c
VThomas-git <thomas.vang92@gmail.com>
2021-02-17 15:25:20 +0100

    Relocate packages into com.tse.mobtracker and extends MobilityRepository.java as it should be

---
+++ mob-tracker/src/main/java/com/tse/mobtracker/repository/MobilityRepository.java
@@ -0,0 +1,7 @@
+package com.tse.mobtracker.repository;
+
+import com.tse.mobtracker.entity.Mobility;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MobilityRepository extends CrudRepository<Mobility, Integer> {
+}
